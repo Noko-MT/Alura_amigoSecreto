cf08c48a8ae87128a5ce18370ea164a26185cd51
Noko-MT <nicolaspenuelalab@gmail.com>
2025-03-18 19:28:41 -0500

    Amigo secreto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto Alura Latam Amigo Secreto 
+-Nicolas David Peñuela Arango
+Este proyecto es un primer paso para consolidar mi logica de programación 
---
+++ app.js
@@ -0,0 +1,20 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+console.log("Hello world  ds")
+
+
+
+//Variables
+//funciones
+//Eventos
+//Objetos
+
+
+//Registra nombres
+const listFrends = [];
+
+
+const Amigo = document.getElementById("amigo");
+
+console.log(Amigo)
