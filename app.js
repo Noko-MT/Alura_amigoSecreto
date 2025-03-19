3941e44089a4d327f0fd07715ea591c6b9cb77b8
Noko-MT <nicolaspenuelalab@gmail.com>
2025-03-18 20:45:00 -0500

    Primer Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-console.log("Hello world  ds")
+//console.log("Hello world  ds")
 
 
 
@@ -11,10 +11,29 @@ console.log("Hello world  ds")
 //Objetos
 
 
-//Registra nombres
-const listFrends = [];
-
-
+//Variables utilizadas
+const listAmigo = [];
 const Amigo = document.getElementById("amigo");
+const seeLista = document.getElementById("listaAmigos");
 
-console.log(Amigo)
+
+function agregarAmigo() {
+    if(!Amigo.value){alert("Debes ingresar un nombre")}
+
+    //listaAmigos
+    listAmigo.push(Amigo.value);
+    seeLista.innerHTML += "<h1>" + Amigo.value + "</h1>";
+
+    //seeLista.innerHTML += "<li>" + Amigo.value + "</li>";
+
+    console.log(listAmigo);
+
+}
+
+function sortearAmigo(){
+    
+    
+    let random = Math.floor(Math.random() * listAmigo.length)
+    //seeLista.innerHTML += "<h1>" + Amigo.value + "</h1>";
+    console.log(random)
+}
